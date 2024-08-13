989b29cbee12ccff869456ed74e47bec19a8fec4
avaninath16 <avani.nath16@gmail.com>
2024-08-13 10:10:58 -0700

    not done yet Update Program4.java

--- Program4.java
+++ Program4.java
@@ -1,5 +1,12 @@
 //Youre on your own!
 //Jk ask for help if you need it (but also look at previous programs!!)
+ import java.util.Scanner; 
+    
+    public class Program4 {
+        public static void main(String[] args){
+            Scanner myScanner=new Scanner (System.in);
+        }
+    }
 
 
 
