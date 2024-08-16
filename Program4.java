9185881ef4b5776e2ce914c33703d694d92d7882
avaninath16 <avani.nath16@gmail.com>
2024-08-16 09:27:50 -0700

    workinprogressUpdate Program4.java

--- Program4.java
+++ Program4.java
@@ -1,14 +1,38 @@
 //Youre on your own!
 //Jk ask for help if you need it (but also look at previous programs!!)
- import java.util.Scanner; 
+
+    import java.util.Scanner; 
     
     public class Program4 {
         public static void main(String[] args){
             Scanner myScanner=new Scanner (System.in);
+            int num1 = 0;
+            int num2 = 0;
+            int num3 = 0;
+            int num4 = 0;
+            int num5 = 0;
+            int num6 = 0;
+            
+            System.out.println ("Enter 1st number: ");
+            
+            num1 = myScanner.nextInt();
+            
+            System.out.println ("Enter 2nd number: ");
+            
+            num2 = myScanner.nextInt();
+            
+            System.out.println ("Enter 3rd number: ");
+            
+            num3 = myScanner.nextInt();
+            
+            System.out.println ("Enter 4th number: ");
+            
+            num4 = myScanner.nextInt();
+            
+            
+            
         }
     }
-
-
 
 //Paste console output below:
 /*
